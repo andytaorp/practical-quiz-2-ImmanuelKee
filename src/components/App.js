77e8708c86d76f9b87fd89f0b7d28a10ac909dd6
ImmanuelKee <23022362@myrp.edu.sg>
2024-11-29 14:39:02 +0800

    test

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,30 @@ function App() {
 
   const handleAddTask = (newTask) => {
     // TODO: write code to add a new task
+    setTasks((prevItems) => [...prevItems, newTask]);
   };
 
   const handleToggleTask = (id) => {
     // TODO: write code to toggle a task's status
+    setTasks((prevItems) => 
+      prevItems.map((task) => 
+      task.id === id ? { ...task, status: !task.status } : task
+      )
+    );
   };
 
   const handleDeleteTask = (id) => {
     // TODO: write code to delete a task
+    setTasks((prevItems) => prevItems.filter(task => task.id !== id));
   };
 
   return (
     <div className="app">
       <h1>Task Tracker</h1>
       {/*TODO: add a form to add a new task*/}
+      <Form onAddTask={handleAddTask} />
       {/*TODO: add a list of tasks*/}
+      <TaskList tasks={tasks} onDeleteTask={handleDeleteTask} onToggleTask={handleToggleTask} />
     </div>
   );
 }
